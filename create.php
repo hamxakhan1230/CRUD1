2ae4e6535ab2a9413727baa36ca5edcf9c61eb26
hamxakhan <hamxakhan1230@gmail.com>
2024-02-22 10:44:09 +0500

    "22/2/24"

---
+++ create.php
@@ -0,0 +1,63 @@
+<?php
+include('dbcon.php');
+if(isset($_POST['submit']))
+{
+    $name= $_POST['name'];
+    $email= $_POST['email'];
+    $age= $_POST['age'];
+    $address= $_POST['address'];
+
+    $sql = "INSERT INTO student (name,email,age,address)
+    values('$name','$email','$age','$address')";
+    $res=mysqli_query($con,$sql); 
+    if($res){
+        echo "data inserted ";
+        header('location:index.php');
+    }else{
+        echo "data not inserted";
+        header('location:index.php');
+    }
+}
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1 class="text-center">crud in php</h1>
+    <div class="container">
+      <form  method="post">
+        <div class="form-group">
+            <label for="">name</label>
+            <input type="text" name="name" id="" class="form-control" placeholder="" >
+        </div>
+        <div class="form-group">
+            <label for="">email</label>
+            <input type="email" name="email" id="" class="form-control" placeholder="" >
+        </div>
+        <div class="form-group">
+            <label for="">age</label>
+            <input type="text" name="age" id="" class="form-control" placeholder="" >
+        </div>
+        <div class="form-group">
+            <label for="">address</label>
+            <input type="text" name="address" id="" class="form-control" placeholder="" >
+        </div>
+        <button class="btn btn-info" type="submit" name ="submit">submit</button>
+      </form>
+      </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
